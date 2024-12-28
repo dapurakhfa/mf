d255e7fc245094b3e9f0a9626e5d783b93097350
dapurakhfa <dapurakhfa@gmail.com>
2024-12-28 20:37:03 +0700

    setup MAIN env

--- apps/docs/next.config.js
+++ apps/docs/next.config.js
@@ -1,9 +1,15 @@
+const { MAIN_URL } = process.env
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   assetPrefix: '/docs-static',
   async rewrites() {
     return {
       beforeFiles: [
+        {
+          source: '/',
+          destination: `${MAIN_URL}/`,
+        },
         // This rewrite is necessary to support assetPrefix only in Next 14 and below.
         // It is not necessary in Next 15.
         {
